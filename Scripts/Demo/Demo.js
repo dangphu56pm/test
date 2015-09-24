73018045f5b2398f487974b67a5ef00619c9e62d
dangphu56pm <dangphu.56pm@gmail.com>
2015-09-24 23:02:06 +0700

    commit
    dfdg

---
+++ Scripts/Demo/Demo.js
@@ -0,0 +1,19 @@
+﻿$('a[href*=#]:not([href=#])').not('.panel a').click(function (e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            if ($(".navbar").css("position") == "fixed") {
+                $('html,body').animate({
+                    scrollTop: target.offset().top = 72
+                }, 700, 'swing');
+            } else {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 700, 'swing');
+                return false;
+            }
+        }
+    }
+});
